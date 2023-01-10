2a3c72a45e211a18ebc979c4c25705922ac76fba
vinay <84218725+vinaykulk32@users.noreply.github.com>
2023-01-10 12:58:24 +0530

    Add files via upload

---
+++ lab-9/changePermissions.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#change the permission on the file workfile.txt
+ls -l workfile.txt
+chmod 711 workfile.txt
+ls -l workfile.txt
+chmod 520 workfile.txt
+ls -l workfile.txt
+chmod 776 workfile.txt
+ls -l workfile.txt
